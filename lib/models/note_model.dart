f9161184bee62eebef3d51c3d2a8e3ed252d3fef
khaledelnaghy <khaledelnaghy777@gmail.com>
2024-12-22 17:49:11 +0200

    init hive and open notes box

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,15 +1,22 @@
-class NoteModel {
-  final String title;
-  final String subtitle;
-  final String date;
-  final int color;
+import 'package:hive/hive.dart';
 
-  NoteModel(
-      {
-      required this.title,
-      required this.subtitle,
-      required this.date,
-      required this.color
-      }
-      );
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class   NoteModel extends HiveObject {
+  @HiveField(0)
+  String title;
+  @HiveField(1)
+  String subTitle;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
 }
